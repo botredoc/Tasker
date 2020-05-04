8ad89c24ba67c4cc1b0222fda7e3b9523703996b
Lokesh Rao <lokeshrao1997@gmail.com>
2020-05-04 15:05:52 +0530

    crashlysis

--- app/src/main/java/com/lk/tasker/MainActivity.kt
+++ app/src/main/java/com/lk/tasker/MainActivity.kt
@@ -2,11 +2,19 @@ package com.lk.tasker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var x:Int
+        x=34
+        var y=x/0
+        Log.e("tesr",y.toString())
+
     }
 }
