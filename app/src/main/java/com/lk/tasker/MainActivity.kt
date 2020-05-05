8ea4df7fa1ed1bef295269bd1383936edf7c058a
Lokesh Rao <lokeshrao1997@gmail.com>
2020-05-05 15:39:42 +0530

    video ad

--- app/src/main/java/com/lk/tasker/MainActivity.kt
+++ app/src/main/java/com/lk/tasker/MainActivity.kt
@@ -7,20 +7,35 @@ import android.view.ViewGroup
 import android.widget.Button
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     lateinit var mAdView : AdView
-
+    lateinit var button:Button
+    private lateinit var mInterstitialAd: InterstitialAd
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button=findViewById(R.id.ads)
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        button.setOnClickListener{
+
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        }
         MobileAds.initialize(this) {
 
         }
         mAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
+
 //        var x:Int
 //        x=34
 //        var y=x/0
