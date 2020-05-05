33d8a88bdb39a1841d5ba6a849ae22ab329c11e6
Lokesh Rao <lokeshrao1997@gmail.com>
2020-05-05 12:57:31 +0530

    admob

--- app/src/main/java/com/lk/tasker/MainActivity.kt
+++ app/src/main/java/com/lk/tasker/MainActivity.kt
@@ -5,16 +5,24 @@ import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var x:Int
-        x=34
-        var y=x/0
-        Log.e("tesr",y.toString())
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+//        var x:Int
+//        x=34
+//        var y=x/0
+//        Log.e("tesr",y.toString())
 
     }
 }
