987c9107946956bf970533f6150889a625231333
Lokesh Rao <lokeshrao1997@gmail.com>
2020-05-16 11:18:09 +0530

    login and home

--- app/src/main/java/com/lk/tasker/splash_screen.kt
+++ app/src/main/java/com/lk/tasker/splash_screen.kt
@@ -10,15 +10,21 @@ import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.lk.tasker.databinding.ActivityLoginBinding
+import com.lk.tasker.databinding.ActivitySplashScreenBinding
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class splash_screen : AppCompatActivity() {
 
     private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
     lateinit var tasker : TextView
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window.decorView.apply {
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
@@ -30,27 +36,32 @@ class splash_screen : AppCompatActivity() {
 //        iconAnimation.setOneShot(false);
 //          iconImage.setBackgroundDrawable(iconAnimation)
  //         iconAnimation.start()
-        val iconImage : ImageView = findViewById(R.id.imageView)
-        iconImage.setBackgroundResource(R.drawable.icon_animation_list)
+        binding.imageView.setBackgroundResource(R.drawable.icon_animation_list)
 
-        val iconAnimation = iconImage.background
-        if(iconAnimation is Animatable){
+        val iconAnimation = binding.imageView.background
+        if(iconAnimation is Animatable) {
+
+            val animDrawable = splash_screen_layout.background as AnimationDrawable
+            animDrawable.setEnterFadeDuration(10)
+            animDrawable.setExitFadeDuration(2000)
+            val stripesAnimation = AnimationUtils.loadAnimation(this, R.anim.strip_animation)
+            stripes.startAnimation(stripesAnimation)
+            animDrawable.start()
             iconAnimation.start()
+            AnimationUtils.loadAnimation(this,R.anim.slide_to_center).also {
+                binding.appName.startAnimation(it)
+            }
+            AnimationUtils.loadAnimation(this,R.anim.fade_out).also {
+                binding.extratext.startAnimation(it)
+
+            }
+
+
         }
-        val animDrawable = splash_screen_layout.background as AnimationDrawable
-        animDrawable.setEnterFadeDuration(10)
-        animDrawable.setExitFadeDuration(2000)
-        animDrawable.start()
-
-
-        val stripesAnimation = AnimationUtils.loadAnimation(this,R.anim.strip_animation)
-        stripes.startAnimation(stripesAnimation)
-
         Handler().postDelayed({
 
             startActivity(Intent(this,loginActivity::class.java))
         //    overridePendingTransition(R.anim.slide_in,R.anim.no_animation)
-            finish()
 
         }, SPLASH_TIME_OUT)
     }
